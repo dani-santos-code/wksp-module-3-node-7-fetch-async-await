44faa115b78b3132d92e9bc216ebc38509267074
Dani Santos <santos.adaniele@gmail.com>
2020-02-25 12:47:14 -0500

    exercise 5

--- __workshop/2-promises-in-action/exercise-5.js
+++ __workshop/2-promises-in-action/exercise-5.js
@@ -1,5 +1,18 @@
-const request = require('request-promise');
+/*
+1. Head over to https://github.com/sameerkumar18/geek-joke-api.
+Read the documentation...
+2. Write a Promise that will return a random joke.
+*/
+
+const request = require("request-promise");
 
 const getGeekJoke = async () => {
-
-}
+  let options = {
+    url: "https://geek-jokes.sameerkumar.website/api",
+    method: "GET"
+  };
+  request(options, (err, response, body) => {
+    if (!err && response.statusCode == 200) console.log(JSON.parse(body));
+  });
+};
+getGeekJoke();
