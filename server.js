218afed076a95882840b9159eafa42ac86e5fd0c
Dani Santos <santos.adaniele@gmail.com>
2020-02-25 11:35:21 -0500

    exercise 0

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 8000;
 
 express()
-    .use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    })
-	.use(morgan('dev'))
-	.use(express.static('public'))
-    .use(bodyParser.json())
-    .use(express.urlencoded({extended: false}))
-    .set('view engine', 'ejs')
+  .use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  })
+  .use(morgan("dev"))
+  .use(express.static("public"))
+  .use(bodyParser.json())
+  .use(express.urlencoded({ extended: false }))
+  .set("view engine", "ejs")
+  .get("/hello", (req, res) => {
+    res.send("Hello, there again!");
+  })
+  // endpoints
 
-    // endpoints
-
-    .listen(PORT, () => console.log(`Listening on port ${PORT}`));
+  .listen(PORT, () => console.log(`Listening on port ${PORT}`));
